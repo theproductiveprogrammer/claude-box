18f671330752639b86c58ed78a08312ab7791944
Charles L <charles.lobo@gmail.com>
2026-09-15 17:41:31 +1000

    feat: use docker instead of podman

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-podman build -t claude-box \
+docker build -t claude-box \
   --build-arg HELIX_REF=gv \
   --build-arg NPM_REGISTRY=https://fsaiartifact.jfrog.io/artifactory/api/npm/atlas-virtual-repository-npm/ \
   --secret id=npmrc,src="$HOME/.npmrc" \
